919e708d0e466083ee1e69ae027fec83266bcc4e
kelsoncm <kelsoncm@gmail.com>
2022-04-18 17:20:49 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def package_data_dirs(root, data_dirs):
 
 setup(
     name='django-theme-adminlte3',
-    version='3.2.0.1',
+    version='3.2.0.2',
     url='https://github.com/kelsoncm/django-theme-adminlte3',
     download_url='https://github.com/kelsoncm/django-theme-adminlte3/releases',
     description='Django Admin LTE v3 Theme',
